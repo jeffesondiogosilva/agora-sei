3a7a2bbdba9978cd6d2dc7e536198d75f480c2f8
jeff <diogoworkss@gmail.com>
2024-08-18 00:01:41 -0300

    feat: forçando idioma portugues no speech

--- src/TextToSpeech.js
+++ src/TextToSpeech.js
@@ -4,6 +4,16 @@ const TextToSpeech = ({ text }) => {
     const handleSpeak = () => {
         if ('speechSynthesis' in window) {
             const utterance = new SpeechSynthesisUtterance(text);
+            utterance.lang = 'pt-BR'; // Define o idioma para português do Brasil
+
+            // Filtra as vozes disponíveis para pt-BR
+            const voices = window.speechSynthesis.getVoices();
+            const portugueseVoices = voices.filter(voice => voice.lang === 'pt-BR');
+
+            if (portugueseVoices.length > 0) {
+                utterance.voice = portugueseVoices[0]; // Seleciona a primeira voz em pt-BR
+            }
+
             window.speechSynthesis.speak(utterance);
         } else {
             alert("Seu navegador não suporta síntese de voz.");
